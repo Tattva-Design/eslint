26ac02289109392d6a1c1fdb1a6a32e417442ed0
dinesh36 <ddinesh.36@gmail.com>
2026-01-07 15:04:36 +0530

    Changes for setting the config file with common config

--- src/index.mjs
+++ src/index.mjs
@@ -2,23 +2,35 @@ import { createRequire } from 'node:module';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { FlatCompat } from '@eslint/eslintrc';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const require = createRequire(import.meta.url);
 
-const {
-    ignores,
-    typescriptConfig,
-    sliceOverride,
-} = require('./common-config.cjs');
+/* eslint-disable import/no-unresolved */
+const typescriptEslintParser = require('@typescript-eslint/parser');
+const typescriptEslintPlugin = require('@typescript-eslint/eslint-plugin');
+const reactPlugin = require('eslint-plugin-react');
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
+const importPlugin = require('eslint-plugin-import');
 
-const compat = new FlatCompat({
-    baseDirectory: __dirname
-});
+import {
+    ignores, files, reduxSliceIgnoreRules, languageOptions, rules,
+} from './common-config.cjs';
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 export default [
-    { ignores },
+    ignores,
     ...compat.extends('airbnb'),
-    typescriptConfig,
-    sliceOverride,
+    {
+        files,
+        languageOptions: { parser: typescriptEslintParser, ...languageOptions },
+        plugins: {
+            '@typescript-eslint': typescriptEslintPlugin,
+            react: reactPlugin,
+            'react-hooks': reactHooksPlugin,
+            import: importPlugin,
+        },
+        rules,
+    },
+    reduxSliceIgnoreRules,
 ];
